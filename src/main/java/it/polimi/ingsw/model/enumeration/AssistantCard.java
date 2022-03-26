e1e634f680fa3ed3789b2a52513ed8d1094e7891
Manu <manuel.ianieri@mail.polimi.it>
2022-03-26 12:57:58 +0100

    commit AssinstCad and Mages

---
+++ src/main/java/it/polimi/ingsw/model/enumeration/AssistantCard.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.model.enumeration;
+
+public class AssistantCard {
+
+    private int turnHeaviness;
+
+    private int movementMotherNature;
+
+    private Mages mage;
+
+    private boolean available=true;
+    //At the beginning all the cards are available=true, when used, the chosen one become available=false.
+
+    public void use(){
+
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/model/enumeration/Mages.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.model.enumeration;
+
+public class Mages {
+
+    public Mages mage1;
+    public Mages mage2;
+    public Mages mage3;
+    public Mages mage4;
+}
