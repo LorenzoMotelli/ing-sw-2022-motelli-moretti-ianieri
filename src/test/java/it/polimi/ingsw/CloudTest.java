540b5324a67f411752bcb7a054ce4a1a998f6f91
LorenzoMotelli <lorenzo.motelli97@gmail.com>
2022-05-20 13:45:34 +0200

    Re-upload missing tests

---
+++ src/test/java/it/polimi/ingsw/CloudTest.java
@@ -0,0 +1,47 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.model.Cloud;
+import it.polimi.ingsw.model.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static it.polimi.ingsw.model.enumeration.PawnColor.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CloudTest {
+    private Cloud cloud;
+
+    @BeforeEach
+    public void setUp(){
+        cloud = new Cloud();
+        Student blueStudent = new Student();
+        Student greenStudent = new Student();
+        Student pinkStudent = new Student();
+        Student redStudent = new Student();
+        Student yellowStudent = new Student();
+        List<Student> studentList = new ArrayList<>();
+        List<Student> students = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            blueStudent.setColor(BLUE);
+            greenStudent.setColor(GREEN);
+            pinkStudent.setColor(PINK);
+            redStudent.setColor(RED);
+            yellowStudent.setColor(YELLOW);
+        }
+        studentList.add(blueStudent);
+        studentList.add(greenStudent);
+        studentList.add(redStudent);
+        cloud.setCloudStudents(studentList);
+    }
+
+    @Test
+    public void getStudents_ShouldReturnStudents(){
+        assertNotNull(cloud.getCloudStudents());
+        assertEquals(BLUE,cloud.getCloudStudents().get(0).getColor());
+        assertEquals(GREEN,cloud.getCloudStudents().get(1).getColor());
+        assertEquals(RED,cloud.getCloudStudents().get(2).getColor());
+    }
+}
